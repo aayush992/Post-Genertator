4f230847582a6bd16b467725b8b16ce84878efbb
aayush992 <132824984+aayush992@users.noreply.github.com>
2024-11-18 00:07:54 +0530

    Update llm_helper.py

--- llm_helper.py
+++ llm_helper.py
@@ -1,17 +1,16 @@
-from dotenv import load_dotenv  # Corrected spelling of `dotenv`
+from dotenv import load_dotenv  
 from langchain_groq import ChatGroq
 import os
 
-# Load the environment variables from the .env file
-load_dotenv()  # Fixed the typo `load dotnev()`
 
-# Initialize ChatGroq with the API key from the environment variables
+load_dotenv()  
+
+
 llm = ChatGroq(
-    groq_api_key=os.getenv("GROQ_API_KEY"),  # Fixed `os.getnev` to `os.getenv`
-    model_name="llama-3.2-90b-text-preview"  # Corrected spelling of "llama" and "preview"
+    groq_api_key=os.getenv("GROQ_API_KEY"), 
+    model_name="llama-3.2-90b-text-preview"  
 )
 
-# Example: Check if the script runs without errors
 if __name__ == "__main__":
     response = llm.invoke("Two most important ingrident in samosa are ")
     print(response.content)
